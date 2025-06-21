5f6b693775f83b66a97c3db5d399182060ef806f
Joseph Habisohn <joebewon507@gmail.com>
2025-06-21 10:37:44 -0500

    Started work on updating QueryOptions
    Making it more user friendly.
    - Allowing direct string input.
    - Adding Getters and Setters
    - Adding TSDocs

--- src/types/api_types.ts
+++ src/types/api_types.ts
@@ -31,25 +31,25 @@ export interface AcumaticaConfig {
 /**
  * An interface for the object passed into the QueryOptions constructor.
  * 
- * @param { string | Filter } $filter `string | Filter` (Optional) A Filter object defining the OData filter paramter. {@link https://help.acumatica.com/(W(5))/Help?ScreenId=ShowWiki&pageid=f279ec05-2f71-4376-a76f-d3ed0e0e2556 Acumatica Docs for $filter}.
- * @param { number } $top `number` (Optional) The number of rows to get. {@link https://help.acumatica.com/(W(5))/Help?ScreenId=ShowWiki&pageid=12ecc8c7-b0c0-4f26-bfe5-388be9674374 Acumatica Docs for $top}.
+ * @param { string | Filter } $filter `string | Filter` (Optional) A Filter object or a filter string that defines the OData filter. {@link https://help.acumatica.com/(W(5))/Help?ScreenId=ShowWiki&pageid=f279ec05-2f71-4376-a76f-d3ed0e0e2556 Acumatica Docs for $filter}.
+ * @param { number } $top `number` (Optional) How many entries to return. Viz. only the first N entries will be returned. {@link https://help.acumatica.com/(W(5))/Help?ScreenId=ShowWiki&pageid=12ecc8c7-b0c0-4f26-bfe5-388be9674374 Acumatica Docs for $top}.
  * @param { number } $skip `number` (Optional) The number of rows to skip before getting data. {@link https://help.acumatica.com/(W(5))/Help?ScreenId=ShowWiki&pageid=be37b0cb-eb17-41c0-944c-d52f7a65cf2e Acumatica Docs for $skip}.
- * @param { string[] } $expand `string[]` (Optional) The list of linked and detail entities to expand. {@link https://help.acumatica.com/(W(5))/Help?ScreenId=ShowWiki&pageid=b6c2fecd-0b2e-4aba-9bad-4c5050befe25 Acumatica Docs for $expand}.
- * @param { string[] } $select `string[]` (Optional) The list of fields to return. {@link https://help.acumatica.com/(W(5))/Help?ScreenId=ShowWiki&pageid=ecbfdb12-4853-4ff5-b8c5-eade02f9b47b Acumatica Docs for $select}.
+ * @param { string | string[] } $expand `string | string[]` (Optional) The list of linked and detail entities to expand. {@link https://help.acumatica.com/(W(5))/Help?ScreenId=ShowWiki&pageid=b6c2fecd-0b2e-4aba-9bad-4c5050befe25 Acumatica Docs for $expand}.
+ * @param { string | string[] } $select `string | string[]` (Optional) The list of fields to return. {@link https://help.acumatica.com/(W(5))/Help?ScreenId=ShowWiki&pageid=ecbfdb12-4853-4ff5-b8c5-eade02f9b47b Acumatica Docs for $select}.
  */
 export interface QueryOptionsConfig {
-    /** (Optional) A Filter object defining the OData filter paramter. {@link https://help.acumatica.com/(W(5))/Help?ScreenId=ShowWiki&pageid=f279ec05-2f71-4376-a76f-d3ed0e0e2556 Acumatica Docs for $filter}. */
+    /** (Optional) A Filter object or a filter string that defines the OData filter. {@link https://help.acumatica.com/(W(5))/Help?ScreenId=ShowWiki&pageid=f279ec05-2f71-4376-a76f-d3ed0e0e2556 Acumatica Docs for $filter}. */
     $filter?: string | Filter ;
     
-    /** (Optional) The number of rows to get. {@link https://help.acumatica.com/(W(5))/Help?ScreenId=ShowWiki&pageid=12ecc8c7-b0c0-4f26-bfe5-388be9674374 Acumatica Docs for $top}. */
+    /** (Optional) How many entries to return. Viz. only the first N entries will be returned. {@link https://help.acumatica.com/(W(5))/Help?ScreenId=ShowWiki&pageid=12ecc8c7-b0c0-4f26-bfe5-388be9674374 Acumatica Docs for $top}. */
     $top?: number;
     
     /** (Optional) The number of rows to skip before getting data. {@link https://help.acumatica.com/(W(5))/Help?ScreenId=ShowWiki&pageid=be37b0cb-eb17-41c0-944c-d52f7a65cf2e Acumatica Docs for $skip}. */
     $skip?: number;
     
     /** (Optional) The list of linked and detail entities to expand. {@link https://help.acumatica.com/(W(5))/Help?ScreenId=ShowWiki&pageid=b6c2fecd-0b2e-4aba-9bad-4c5050befe25 Acumatica Docs for $expand}. */
-    $expand?: string[];
+    $expand?: string | string[];
     
     /** (Optional) The list of fields to return. {@link https://help.acumatica.com/(W(5))/Help?ScreenId=ShowWiki&pageid=ecbfdb12-4853-4ff5-b8c5-eade02f9b47b Acumatica Docs for $select}. */
-    $select?: string[];
+    $select?: string | string[];
 }
--- src/utils/QueryOptions.ts
+++ src/utils/QueryOptions.ts
@@ -1,7 +1,11 @@
+import { Filter } from './Filter'
 import { QueryOptionsConfig } from "../types";
 
+/**
+ * A class to encapsulate the OData Query Parameters
+ */
 export class QueryOptions {
-    private params: {
+    #_params: {
         $filter?: string;
         $top?: number;
         $skip?: number;
@@ -14,15 +18,15 @@ export class QueryOptions {
      */
     constructor(options: QueryOptionsConfig) {
         if (options.$top !== undefined && options.$skip !== undefined && (!Number.isInteger(options.$top) || !Number.isInteger(options.$skip))) {
-            throw new Error("Both options.$top and options.$skip must be integers.");
+            throw new TypeError("Both options.$top and options.$skip must be integers.");
         }
         
-        this.params = {
+        this.#_params = {
             $filter: options.$filter?.toString(),
             $top: options.$top,
             $skip: options.$skip,
-            $expand: options.$expand?.join(','),
-            $select: options.$select?.join(',')
+            $expand: typeof options.$expand === "string" ? options.$expand : options.$expand?.join(','),
+            $select: typeof options.$select === "string" ? options.$select : options.$select?.join(',')
         };
     }
 
@@ -34,10 +38,72 @@ export class QueryOptions {
      */
     public build(url: string = ""): string {
         let param_list: string[] = [];
-        for (let [key, value] of Object.entries(this.params)) {
+        for (let [key, value] of Object.entries(this.#_params)) {
             if (key !== undefined && value !== undefined) param_list.push(`${key}=${value}`);
         }
 
         return `${url}?${param_list.join('&')}`;
+    }
+
+    // ------------------- ------------------- Getters and Setters ------------------- ------------------- \\
+    // ------------------- $filter ------------------- \\
+    
+    public getFilter(): string | undefined {
+        return this.#_params.$filter;
+    }
+
+    /**
+     * Sets the `$filter` query parameter.
+     * 
+     * @param { string | Filter } flt `string | Filter` A Filter object defining the OData filter paramter or the filter string itself. {@link https://help.acumatica.com/(W(5))/Help?ScreenId=ShowWiki&pageid=f279ec05-2f71-4376-a76f-d3ed0e0e2556 Acumatica Docs for $filter}.
+     */
+    public setFilter(flt: string | Filter): void {
+        this.#_params.$filter = flt.toString();
+    }
+
+    /**
+     * Getter for `$top`.
+     * 
+     * @returns `number` What is set in the `$top` query paramter.
+     */
+    public getTop(): number {
+        return this.#_params.$top;
+    }
+
+    /**
+     * Sets the `$top` query parameter.
+     * 
+     * @param { number } n `number` How many entries to return. Viz. only the first `@n` entries will be returned. {@link https://help.acumatica.com/(W(5))/Help?ScreenId=ShowWiki&pageid=12ecc8c7-b0c0-4f26-bfe5-388be9674374 Acumatica Docs for $top}.
+     * @throws `TypeError` When `@n` is not an integer.
+     */
+    public setTop(n: number): void {
+        if (!Number.isInteger(n)) {
+            throw new TypeError("Expected @n to be an integer.");
+        }
+        
+        this.#_params.$top = n;
+    }
+
+    /**
+     * Getter for `$skip`.
+     * 
+     * @returns `number` What is set in the `$skip` query paramter.
+     */
+    public getSkip(): number {
+        return this.#_params.$skip;
+    }
+
+    /**
+     * Sets the `$top` query parameter.
+     * 
+     * @param { number } n `number` The number of rows to skip before getting data. {@link https://help.acumatica.com/(W(5))/Help?ScreenId=ShowWiki&pageid=be37b0cb-eb17-41c0-944c-d52f7a65cf2e Acumatica Docs for $skip}.
+     * @throws `TypeError` When `@n` is not an integer.
+     */
+    public setSkip(n: number): void {
+        if (!Number.isInteger(n)) {
+            throw new TypeError("Expected @n to be an integer.");
+        }
+        
+        this.#_params.$skip = n;
     }
 }
